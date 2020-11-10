ff951ddc415a5d424a7341304a9f6d446e88727e
Kirpa Vadim <ivadik2323@gmail.com>
2020-11-10 10:38:18 +1000

    Add length and complexity in ApiTitleInfo

--- thirdparty_api/kinopoisk_api.py
+++ thirdparty_api/kinopoisk_api.py
@@ -6,6 +6,16 @@ def get_id_from_url(url):
     r = r'^.*?kinopoisk.ru/film/(\d+)'
     return re.search(r, url)[1]
 
+def calc_complexity(score, minutes):
+    MAX_TIME = 60*3 # 3 hr movie
+    hardness_to_watch = (max(1, min(9 - score, 8)) - 1) / 7
+    complexity = hardness_to_watch * (minutes / MAX_TIME) * 100
+    return int(complexity)
+
+def length_to_minutes(length):
+    parts = length.split(":")
+    return int(parts[0]) * 60 + int(parts[1]) # hope it works
+
 def get_film_data(url, token, tables=['RATING']):
     id = get_id_from_url(url)
     headers = {'X-API-KEY': token, 'accept': 'application/json'}
